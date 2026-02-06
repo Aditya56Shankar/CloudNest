fac1f7cba026745d5c38aaec5a9fd64ebe12c5d3
Divyansh Goyal <122771637+jaishriram1@users.noreply.github.com>
2026-02-06 23:30:10 +0530

    Delete CloudNest-main/CloudNest-main directory

--- CloudNest-main/CloudNest-main/client/src/routes/home.jsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "../components/sign-up-form";
-
-export default function Home() {
-  return (
-    <div className="flex h-svh items-center justify-center">
-      <SignUpForm />
-    </div>
-  );
-}
--- CloudNest-main/CloudNest-main/server/src/utils/pdfParse.cjs
+++
@@ -1,5 +0,0 @@
-const pdfParse = require("pdf-parse");
-
-module.exports = function parsePdf(buffer) {
-  return pdfParse(buffer);
-};
